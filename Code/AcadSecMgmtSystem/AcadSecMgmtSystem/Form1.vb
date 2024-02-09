fd7bad5d393e30441e06201a60e8604b49df782d
dhaneshragu <dhanesh.v@iitg.ac.in>
2024-02-09 20:34:42 +0530

    basic setup done

---
+++ Code/AcadSecMgmtSystem/AcadSecMgmtSystem/Form1.vb
@@ -0,0 +1,27 @@
+﻿Imports System.Data.SqlClient
+Imports System.IO
+
+Public Class Form1
+    Dim conString = Globals.getdbConnectionString()
+    Dim Con = New SqlConnection(conString)
+    Dim query = "SELECT * from StudentDetails"
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        Con.Open()
+        Label1.Text = "Success"
+        Dim cmd As New SqlCommand(query, Con)
+        Dim adapter As New SqlDataAdapter(cmd)
+
+        ' Create a DataTable to store the data
+        Dim dataTable As New DataTable()
+
+        ' Fill the DataTable with data from the SQL table
+        adapter.Fill(dataTable)
+        ' Bind the DataTable to the DataGridView
+        DataGridView1.DataSource = dataTable
+        ' Optionally, enable sorting
+        DataGridView1.Columns("Name").SortMode = DataGridViewColumnSortMode.Automatic
+        Con.Close()
+    End Sub
+
+
+End Class
---
+++ Code/AcadSecMgmtSystem/AcadSecMgmtSystem/Globals.vb
@@ -0,0 +1,12 @@
+﻿Imports System.Configuration
+
+'To get the global variables/declarations to be used all over the project
+Public Class Globals
+    Private Shared dbConnectionString As String = ConfigurationManager.ConnectionStrings("ConString").ConnectionString
+
+    'To get the dbString
+    Public Shared Function getdbConnectionString() As String
+        Return dbConnectionString
+    End Function
+
+End Class
