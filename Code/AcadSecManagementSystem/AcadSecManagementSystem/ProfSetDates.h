3d1b53fc1fd00068142560b7fbdb8872fa63d2d6
dhaneshragu <dhanesh.v@iitg.ac.in>
2024-02-16 15:39:03 +0530

    minor fix

--- Code/AcadSecManagementSystem/AcadSecManagementSystem/ProfSetDates.h
+++ Code/AcadSecManagementSystem/AcadSecManagementSystem/ProfSetDates.h
@@ -123,86 +123,86 @@ namespace AcadSecManagementSystem {
 			// 
 			this->label1->AutoSize = true;
 			this->label1->ForeColor = System::Drawing::Color::Blue;
-			this->label1->Location = System::Drawing::Point(245, 120);
+			this->label1->Location = System::Drawing::Point(245, 104);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(210, 24);
+			this->label1->Size = System::Drawing::Size(223, 24);
 			this->label1->TabIndex = 0;
-			this->label1->Text = L"Start Grade Collection";
+			this->label1->Text = L"Start Grade Collection :";
 			// 
 			// label2
 			// 
 			this->label2->AutoSize = true;
 			this->label2->ForeColor = System::Drawing::Color::Blue;
-			this->label2->Location = System::Drawing::Point(287, 164);
+			this->label2->Location = System::Drawing::Point(334, 150);
 			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(121, 24);
+			this->label2->Size = System::Drawing::Size(134, 24);
 			this->label2->TabIndex = 1;
-			this->label2->Text = L"View Grades";
+			this->label2->Text = L"View Grades :";
 			// 
 			// label3
 			// 
 			this->label3->AutoSize = true;
 			this->label3->ForeColor = System::Drawing::Color::Blue;
-			this->label3->Location = System::Drawing::Point(235, 208);
+			this->label3->Location = System::Drawing::Point(220, 193);
 			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(235, 24);
+			this->label3->Size = System::Drawing::Size(248, 24);
 			this->label3->TabIndex = 2;
-			this->label3->Text = L"Start Course Registration";
+			this->label3->Text = L"Start Course Registration :";
 			// 
 			// label4
 			// 
 			this->label4->AutoSize = true;
 			this->label4->ForeColor = System::Drawing::Color::Blue;
-			this->label4->Location = System::Drawing::Point(267, 292);
+			this->label4->Location = System::Drawing::Point(282, 273);
 			this->label4->Name = L"label4";
-			this->label4->Size = System::Drawing::Size(173, 24);
+			this->label4->Size = System::Drawing::Size(186, 24);
 			this->label4->TabIndex = 3;
-			this->label4->Text = L"Midsem Start Date";
+			this->label4->Text = L"Midsem Start Date :";
 			// 
 			// label5
 			// 
 			this->label5->AutoSize = true;
 			this->label5->ForeColor = System::Drawing::Color::Blue;
-			this->label5->Location = System::Drawing::Point(265, 337);
+			this->label5->Location = System::Drawing::Point(280, 321);
 			this->label5->Name = L"label5";
-			this->label5->Size = System::Drawing::Size(175, 24);
+			this->label5->Size = System::Drawing::Size(188, 24);
 			this->label5->TabIndex = 4;
-			this->label5->Text = L"Endsem Start Date";
+			this->label5->Text = L"Endsem Start Date :";
 			// 
 			// label6
 			// 
 			this->label6->AutoSize = true;
 			this->label6->ForeColor = System::Drawing::Color::Blue;
-			this->label6->Location = System::Drawing::Point(273, 425);
+			this->label6->Location = System::Drawing::Point(301, 405);
 			this->label6->Name = L"label6";
-			this->label6->Size = System::Drawing::Size(154, 24);
+			this->label6->Size = System::Drawing::Size(167, 24);
 			this->label6->TabIndex = 5;
-			this->label6->Text = L"Exam Timetable";
+			this->label6->Text = L"Exam Timetable :";
 			// 
 			// label7
 			// 
 			this->label7->AutoSize = true;
 			this->label7->ForeColor = System::Drawing::Color::Blue;
-			this->label7->Location = System::Drawing::Point(300, 384);
+			this->label7->Location = System::Drawing::Point(288, 362);
 			this->label7->Name = L"label7";
-			this->label7->Size = System::Drawing::Size(100, 24);
+			this->label7->Size = System::Drawing::Size(180, 24);
 			this->label7->TabIndex = 6;
-			this->label7->Text = L"Timetable";
+			this->label7->Text = L"Course Timetable :";
 			// 
 			// label8
 			// 
 			this->label8->AutoSize = true;
 			this->label8->ForeColor = System::Drawing::Color::Blue;
-			this->label8->Location = System::Drawing::Point(287, 249);
+			this->label8->Location = System::Drawing::Point(281, 231);
 			this->label8->Name = L"label8";
-			this->label8->Size = System::Drawing::Size(124, 24);
+			this->label8->Size = System::Drawing::Size(187, 24);
 			this->label8->TabIndex = 7;
-			this->label8->Text = L"Fee Payment";
+			this->label8->Text = L"Start Fee Payment :";
 			// 
 			// YesGradeCollection
 			// 
 			this->YesGradeCollection->AutoSize = true;
-			this->YesGradeCollection->Location = System::Drawing::Point(509, 119);
+			this->YesGradeCollection->Location = System::Drawing::Point(509, 103);
 			this->YesGradeCollection->Name = L"YesGradeCollection";
 			this->YesGradeCollection->Size = System::Drawing::Size(61, 28);
 			this->YesGradeCollection->TabIndex = 8;
@@ -213,7 +213,7 @@ namespace AcadSecManagementSystem {
 			// NoGradeCollection
 			// 
 			this->NoGradeCollection->AutoSize = true;
-			this->NoGradeCollection->Location = System::Drawing::Point(674, 119);
+			this->NoGradeCollection->Location = System::Drawing::Point(674, 103);
 			this->NoGradeCollection->Name = L"NoGradeCollection";
 			this->NoGradeCollection->Size = System::Drawing::Size(55, 28);
 			this->NoGradeCollection->TabIndex = 9;
@@ -224,7 +224,7 @@ namespace AcadSecManagementSystem {
 			// NoViewGrades
 			// 
 			this->NoViewGrades->AutoSize = true;
-			this->NoViewGrades->Location = System::Drawing::Point(674, 162);
+			this->NoViewGrades->Location = System::Drawing::Point(674, 146);
 			this->NoViewGrades->Name = L"NoViewGrades";
 			this->NoViewGrades->Size = System::Drawing::Size(55, 28);
 			this->NoViewGrades->TabIndex = 11;
@@ -235,7 +235,7 @@ namespace AcadSecManagementSystem {
 			// YesViewGrades
 			// 
 			this->YesViewGrades->AutoSize = true;
-			this->YesViewGrades->Location = System::Drawing::Point(509, 162);
+			this->YesViewGrades->Location = System::Drawing::Point(509, 146);
 			this->YesViewGrades->Name = L"YesViewGrades";
 			this->YesViewGrades->Size = System::Drawing::Size(61, 28);
 			this->YesViewGrades->TabIndex = 10;
@@ -246,7 +246,7 @@ namespace AcadSecManagementSystem {
 			// NoCourseEnr
 			// 
 			this->NoCourseEnr->AutoSize = true;
-			this->NoCourseEnr->Location = System::Drawing::Point(674, 206);
+			this->NoCourseEnr->Location = System::Drawing::Point(674, 190);
 			this->NoCourseEnr->Name = L"NoCourseEnr";
 			this->NoCourseEnr->Size = System::Drawing::Size(55, 28);
 			this->NoCourseEnr->TabIndex = 13;
@@ -257,7 +257,7 @@ namespace AcadSecManagementSystem {
 			// YesCourseEnr
 			// 
 			this->YesCourseEnr->AutoSize = true;
-			this->YesCourseEnr->Location = System::Drawing::Point(509, 206);
+			this->YesCourseEnr->Location = System::Drawing::Point(509, 190);
 			this->YesCourseEnr->Name = L"YesCourseEnr";
 			this->YesCourseEnr->Size = System::Drawing::Size(61, 28);
 			this->YesCourseEnr->TabIndex = 12;
@@ -267,14 +267,14 @@ namespace AcadSecManagementSystem {
 			// 
 			// dateMidsem
 			// 
-			this->dateMidsem->Location = System::Drawing::Point(514, 289);
+			this->dateMidsem->Location = System::Drawing::Point(514, 273);
 			this->dateMidsem->Name = L"dateMidsem";
 			this->dateMidsem->Size = System::Drawing::Size(200, 28);
 			this->dateMidsem->TabIndex = 14;
 			// 
 			// dateEndsem
 			// 
-			this->dateEndsem->Location = System::Drawing::Point(515, 333);
+			this->dateEndsem->Location = System::Drawing::Point(515, 317);
 			this->dateEndsem->Name = L"dateEndsem";
 			this->dateEndsem->Size = System::Drawing::Size(200, 28);
 			this->dateEndsem->TabIndex = 15;
@@ -282,17 +282,20 @@ namespace AcadSecManagementSystem {
 			// buttonTT
 			// 
 			this->buttonTT->BackColor = System::Drawing::Color::Lime;
-			this->buttonTT->Location = System::Drawing::Point(554, 378);
+			this->buttonTT->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->buttonTT->Location = System::Drawing::Point(515, 356);
 			this->buttonTT->Name = L"buttonTT";
 			this->buttonTT->Size = System::Drawing::Size(124, 34);
 			this->buttonTT->TabIndex = 16;
 			this->buttonTT->Text = L"Generate";
 			this->buttonTT->UseVisualStyleBackColor = false;
+			this->buttonTT->Click += gcnew System::EventHandler(this, &ProfSetDates::buttonTT_Click);
 			// 
 			// buttonExamTT
 			// 
 			this->buttonExamTT->BackColor = System::Drawing::Color::Lime;
-			this->buttonExamTT->Location = System::Drawing::Point(554, 421);
+			this->buttonExamTT->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->buttonExamTT->Location = System::Drawing::Point(515, 399);
 			this->buttonExamTT->Name = L"buttonExamTT";
 			this->buttonExamTT->Size = System::Drawing::Size(124, 34);
 			this->buttonExamTT->TabIndex = 19;
@@ -302,7 +305,7 @@ namespace AcadSecManagementSystem {
 			// NoFeePayment
 			// 
 			this->NoFeePayment->AutoSize = true;
-			this->NoFeePayment->Location = System::Drawing::Point(674, 247);
+			this->NoFeePayment->Location = System::Drawing::Point(674, 231);
 			this->NoFeePayment->Name = L"NoFeePayment";
 			this->NoFeePayment->Size = System::Drawing::Size(55, 28);
 			this->NoFeePayment->TabIndex = 21;
@@ -313,7 +316,7 @@ namespace AcadSecManagementSystem {
 			// YesFeePayment
 			// 
 			this->YesFeePayment->AutoSize = true;
-			this->YesFeePayment->Location = System::Drawing::Point(509, 247);
+			this->YesFeePayment->Location = System::Drawing::Point(509, 231);
 			this->YesFeePayment->Name = L"YesFeePayment";
 			this->YesFeePayment->Size = System::Drawing::Size(61, 28);
 			this->YesFeePayment->TabIndex = 20;
@@ -324,12 +327,14 @@ namespace AcadSecManagementSystem {
 			// label9
 			// 
 			this->label9->AutoSize = true;
-			this->label9->Font = (gcnew System::Drawing::Font(L"Trebuchet MS", 20.8F, System::Drawing::FontStyle::Bold));
+			this->label9->Font = (gcnew System::Drawing::Font(L"Verdana", 18, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label9->ForeColor = System::Drawing::Color::DarkCyan;
 			this->label9->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"label9.Image")));
 			this->label9->ImageAlign = System::Drawing::ContentAlignment::TopLeft;
-			this->label9->Location = System::Drawing::Point(395, 38);
+			this->label9->Location = System::Drawing::Point(388, 31);
 			this->label9->Name = L"label9";
-			this->label9->Size = System::Drawing::Size(249, 43);
+			this->label9->Size = System::Drawing::Size(247, 36);
 			this->label9->TabIndex = 22;
 			this->label9->Text = L"       Set Dates";
 			this->label9->Click += gcnew System::EventHandler(this, &ProfSetDates::label9_Click);
@@ -337,7 +342,7 @@ namespace AcadSecManagementSystem {
 			// buttonSave
 			// 
 			this->buttonSave->BackColor = System::Drawing::Color::GreenYellow;
-			this->buttonSave->Location = System::Drawing::Point(516, 483);
+			this->buttonSave->Location = System::Drawing::Point(344, 480);
 			this->buttonSave->Name = L"buttonSave";
 			this->buttonSave->Size = System::Drawing::Size(138, 47);
 			this->buttonSave->TabIndex = 23;
@@ -347,7 +352,7 @@ namespace AcadSecManagementSystem {
 			// buttonReset
 			// 
 			this->buttonReset->BackColor = System::Drawing::Color::Tomato;
-			this->buttonReset->Location = System::Drawing::Point(334, 483);
+			this->buttonReset->Location = System::Drawing::Point(509, 480);
 			this->buttonReset->Name = L"buttonReset";
 			this->buttonReset->Size = System::Drawing::Size(138, 47);
 			this->buttonReset->TabIndex = 24;
@@ -395,6 +400,9 @@ namespace AcadSecManagementSystem {
 		}
 #pragma endregion
 private: System::Void label9_Click(System::Object^  sender, System::EventArgs^  e) {
+}
+
+private: System::Void buttonTT_Click(System::Object^  sender, System::EventArgs^  e) {
 }
 };
 }
