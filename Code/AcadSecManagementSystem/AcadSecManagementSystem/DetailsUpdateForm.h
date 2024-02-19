b4efdca782fcd0b1f2f3b72b706fa0ac9dce96ba
v-tanish012 <t.tanish@iitg.ac.in>
2024-02-19 12:49:47 +0530

    ui fix

--- Code/AcadSecManagementSystem/AcadSecManagementSystem/DetailsUpdateForm.h
+++ Code/AcadSecManagementSystem/AcadSecManagementSystem/DetailsUpdateForm.h
@@ -315,7 +315,7 @@ namespace AcadSecManagementSystem {
 			// 
 			this->button1->Anchor = System::Windows::Forms::AnchorStyles::Top;
 			this->button1->BackColor = System::Drawing::Color::LightBlue;
-			this->button1->Location = System::Drawing::Point(398, 415);
+			this->button1->Location = System::Drawing::Point(410, 409);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(184, 55);
 			this->button1->TabIndex = 36;
