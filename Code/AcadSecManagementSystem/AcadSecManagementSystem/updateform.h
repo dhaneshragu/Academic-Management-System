e2fdae6c220ee9add2c3422f7488305d0d3d7eb3
Tanish <106595510+v-tanish012@users.noreply.github.com>
2024-03-02 19:30:06 +0530

    Revert "Update updateform.h"
    This reverts commit 4acad6a4a6e50edd22279a3661b3fc7dd910ec4d.

--- Code/AcadSecManagementSystem/AcadSecManagementSystem/updateform.h
+++ Code/AcadSecManagementSystem/AcadSecManagementSystem/updateform.h
@@ -1,13 +1,8 @@
-#ifndef UPDATEFORM_H  
+#ifndef UPDATEFORM_H  // Make it end with _H
 #define UPDATEFORM_H 
 
 #pragma once
-#include <map>  // Add any #include<map> or any libraries you want
-#include <regex> // Add any additional libraries you need
-#include <iostream>
-#include <sstream>
-#include <ctime>
-#include <iomanip>
+// Add any #include<map> or any libraries u want
 using namespace System;
 using namespace System::ComponentModel;
 using namespace System::Collections;
@@ -16,28 +11,14 @@ using namespace System::Data;
 using namespace System::Data::SqlClient;
 using namespace System::Drawing;
 
-// Function to fetch user details by roll number based on the role (Student/Professor)
-std::map<std::string, std::string> FetchDetailsByRollNumber(String^ rollNumber, String^ Role);
 
-// Function to convert System::String^ to std::string
+// IMPORTANT: Function Signatures of all the functions you are making it in the SampleHeader.cpp file
+std::map <std::string, std::string> FetchDetailsByRollNumber(String ^ rollNumber,String ^ Role);
 String^ ConvertStdStringToSystemString(const std::string& stdString);
-
-// Function to parse a date string into a System::DateTime object
 System::DateTime ParseDateString(std::string dateString);
-
-// Function to convert System::String^ to std::string
-void MarshalString(String^ s, std::string& os);
-
-// Function to update user details in the database
-void updateUserDetails(String^ rollnumber, String^ address, String^ password, String^ phoneNo, String^ dateOfBirth, String^ Role);
-
-// Function to check if a phone number is in a valid format
+void MarshalString(String ^ s, std::string& os);
+void updateUserDetails(String ^rollnumber, String ^ address, String ^password, String ^phoneNo, String ^dateOfBirth, Bitmap^ Image, String ^ Role);
 bool IsValidPhoneNumber(String^ phoneNo);
-
-// Function to split a string and return the first part
 std::string splitStringAndReturnFirstPart(const std::string& inputString);
-
-// Function to get the next date based on the current date and number of days ahead
 std::string getNextDate(const std::string &currentDate, int daysAhead);
-
 #endif
